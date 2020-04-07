ec755e2f1f75415b5ce6e0d969b09c351460dfbe
anisiqbal <anisiqbal484@gmail.com>
2020-04-07 13:42:39 +0500

    dashboard/home convert data in loop

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,33 +1,46 @@
 import React from 'react';
 
 function Dashboard() {
+
+  // cards data
+  const cardsData=[
+    {
+      number:'120',
+      name:'Bookings',
+    },
+    {
+      number:'17',
+      name:'Vehicles',
+    },
+    {
+      number:'359',
+      name:'Users',
+    },
+  ]
+  
   return (
     <div className="dashboard">
         <div className="row">
-          <div className="col-md-4">
-            <div className="card">
-              <div className="card-body">
-                <h5 className="card-title">120</h5>
-                <p className="card-text">Bookings</p>
+
+         
+
+         { 
+          cardsData.map(
+            (card, i) => {
+              return(
+                 <div className="col-md-4">
+                <div className="card">
+                <div className="card-body">
+              <h5 className="card-title"> {card.number} </h5>
+                <p className="card-text">{card.name}</p>
               </div>
             </div>
           </div>
-          <div className="col-md-4">
-            <div className="card">
-              <div className="card-body">
-                <h5 className="card-title">17</h5>
-                <p className="card-text">Vehicles</p>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-4">
-            <div className="card">
-              <div className="card-body">
-                <h5 className="card-title">359</h5>
-                <p className="card-text">Users</p>
-              </div>
-            </div>
-          </div>
+              )
+            }
+          )
+         }
+
         </div>
 
         <div className="row pt-5">
